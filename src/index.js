fc6df91fba04ff078e5751fca16e40c40dbef263
Mmvs487 <109397475+Mmvs487@users.noreply.github.com>
2022-07-18 21:16:30 -0500

    Dependencias

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import validator from './validator.js';
+ar datosTarjeta = $('#datosTarjeta');
+function soloNumeros(evt) {
+    evt = (evt) ? evt : window.event;
+    let charCode = (evt.which) ? evt.which : evt.keyCode;
+    if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
+      evt.preventDefault();
+    } else {
+      return true;
+    }
+  }
+
+  function mascaraTarjeta(evt) {
+    evt = (evt) ? evt : window.event;
+    let numeroTarjeta = document.getElementById("numeroTarjeta").value
+    if (numeroTarjeta.length <= 4) {
+      return numeroTarjeta;
+    } else {
+      var masked =
+        numeroTarjeta.substring(0, numeroTarjeta.length - 4).replace(/\d/g, "#") +
+        numeroTarjeta.substring(numeroTarjeta.length - 4, numeroTarjeta.length);
+      return masked;
+    }
+  }
 
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -2,4 +2,17 @@ const validator = {
   // ...
 };
 
+function soloNumeros(evt) {
+  evt = (evt) ? evt : window.event;
+  let charCode = (evt.which) ? evt.which : evt.keyCode;
+  if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
+    evt.preventDefault();
+  } else {
+    return true;
+  }
+}
+import validator from './validator.js';
+
+console.log(validator);
+
 export default validator;
