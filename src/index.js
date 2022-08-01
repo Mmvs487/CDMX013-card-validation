345939845c8e38de2890194bbf0ef15ed842141c
Mmvs487 <109397475+Mmvs487@users.noreply.github.com>
2022-08-01 13:30:17 -0500

    función maskify completa

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
-import validator from './validator.js';
-ar datosTarjeta = $('#datosTarjeta');
-function soloNumeros(evt) {
-    evt = (evt) ? evt : window.event;
-    let charCode = (evt.which) ? evt.which : evt.keyCode;
-    if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
-      evt.preventDefault();
-    } else {
-      return true;
-    }
-  }
+const numeroTarjeta = document.getElementById("numeroTarjeta")
 
-  function mascaraTarjeta(evt) {
-    evt = (evt) ? evt : window.event;
-    let numeroTarjeta = document.getElementById("numeroTarjeta").value
-    if (numeroTarjeta.length <= 4) {
-      return numeroTarjeta;
-    } else {
-      var masked =
-        numeroTarjeta.substring(0, numeroTarjeta.length - 4).replace(/\d/g, "#") +
-        numeroTarjeta.substring(numeroTarjeta.length - 4, numeroTarjeta.length);
-      return masked;
-    }
-  }
+//Sólo digitos:
+numeroTarjeta.addEventListener('keyup', digitsOnly)
+function digitsOnly () {
+let digitsOnly = document.getElementById('numeroTarjeta')
+digitsOnly.value = digitsOnly.value
+.replace(/\s/g, '')
+.replace(/\D/g, '')
+//.replace(/([0-9]{4})/g, '$1-');
+}
 
-console.log(validator);
+
+// No más de 16 caracteres:
+//numeroTarjeta.addEventListener('keyup', () => {
+//  if (numeroTarjeta.value.length > 16) {
+//      numeroTarjeta.value = numeroTarjeta.value.slice(0, 16);
+//  }
+//})
+
+//Enmascarado:
+//función de enmascarado
+function maskify (cc) {
+  return cc.split('').map((letras, idx) => idx < cc.length - 4 ? '#':letras);
+}
+//implementando la función de enmascarado en cuanto se alcanzan 16 dígitos
+numeroTarjeta.addEventListener('keyup', (e) => {
+  let numeroTarjeta = e.target.value;
+  console.log(maskify(numeroTarjeta).join(''));
+  if (numeroTarjeta.length==16) {
+    document.getElementById("numeroTarjeta").value = maskify(numeroTarjeta).join('')
+  }
+});
+
+//Validar el número de la tarjeta:
+//función de validación:
+function validation () {
+  
+}
